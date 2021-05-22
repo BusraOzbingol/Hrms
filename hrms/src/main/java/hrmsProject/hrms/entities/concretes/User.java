3dc3bfaaa96ef1a27e631f77dfb920e87a3187f6
BusraOzbingol <arastirmaprojesi@hotmail.com>
2021-05-22 18:37:11 +0300

    .

---
+++ hrms/src/main/java/hrmsProject/hrms/entities/concretes/User.java
@@ -0,0 +1,31 @@
+package hrmsProject.hrms.entities.concretes;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="users")
+@Data
+public class User {
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	@Column(name="email")
+	private String email;
+	@Column(name="password")
+	private String password;
+	@Column(name="createDate")
+	private Date createDate;
+	@Column(name="active")
+	private boolean active;
+
+
+}
