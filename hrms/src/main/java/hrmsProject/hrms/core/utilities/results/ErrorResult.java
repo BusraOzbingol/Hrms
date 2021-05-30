ef3895183d9e6c383c3051d03137a3219e76e70f
BusraOzbingol <arastirmaprojesi@hotmail.com>
2021-05-30 15:34:14 +0300

    .

---
+++ hrms/src/main/java/hrmsProject/hrms/core/utilities/results/ErrorResult.java
@@ -0,0 +1,11 @@
+package hrmsProject.hrms.core.utilities.results;
+
+public class ErrorResult extends Result{
+	public ErrorResult() {
+		super(false);
+	} 
+	
+	public ErrorResult(String message) {
+		super(false,message);
+	} 
+}
